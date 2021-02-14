834e869343eab3274e5c8c5de8719763a6cad208
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-02-14 09:34:17 -0800

    generates html and outputs to html file

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const Manager = require('./lib/Manager')
 
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
 
 const team = []
 
-function createManager() {
+const managerIcon = 'mug-hot'
+const glassesIcon = 'glasses'
+const internIcon = 'user-graduate'
+
+function createTeam() {
   inquirer
     .prompt([
       {
@@ -82,9 +79,7 @@ function createTeamMember() {
       } else if (
         response.teamMember === "I don't want to add any more team members"
       ) {
-        console.log('no more teammates to add')
-        console.log(team)
-        //createHTML();
+        generateHtml()
       }
     })
 }
@@ -110,7 +105,7 @@ function addEngineer() {
       {
         name: 'github',
         type: 'input',
-        message: "What is this engineer's Github profile?",
+        message: "What is this engineer's Github profile user name?",
       },
     ])
     .then((response) => {
@@ -121,7 +116,6 @@ function addEngineer() {
         response.github
       )
       team.push(engineer)
-      console.log(team)
       createTeamMember()
     })
 }
@@ -163,4 +157,84 @@ function addIntern() {
     })
 }
 
-createManager()
+function generateHtml() {
+  const html = []
+  const htmlBoilerPlate = `
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+  <title>Team Profile</title>
+</head>
+
+<body>
+  <div class="container p-4">
+    <header class="page-header p-4 bg-danger">
+      <h3 class="text-center text-light">My Team
+      </h3>
+    </header>
+  </div>
+  <div class="container px-5">
+    <div class="row justify-content-center">`
+
+  const htmlClosingTags = `
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+  integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+  crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+  crossorigin="anonymous"></script>
+</body>
+</html>`
+  html.push(htmlBoilerPlate)
+
+  for (let teammate of team) {
+    let moreInfo
+    if (teammate.officeNumber) {
+      moreInfo = `Office Number: ${teammate.officeNumber}`
+    }
+    if (teammate.school) {
+      moreInfo = `School: ${teammate.school}`
+    }
+    if (teammate.github) {
+      moreInfo = `GitHub: <span><a href="https://github.com/${teammate.github}">${teammate.github}`
+    }
+    let teammateCard = `
+    <div class="col-sm-4 pb-4">
+    <div class="card shadow mx-4">
+      <div class="card-body bg-primary text-light">
+        <h5 class="card-title"><span><i class="fas fa-${teammate.icon}"></i></span> ${teammate.name}</h5>
+        <p class="card-text">${teammate.role}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: <span>Employee ID: ${teammate.id}</span></li>
+        <li class="list-group-item">Email: <span><a href="mailto:${teammate.email}">${teammate.email}</a></span></li>
+        <li class="list-group-item">${moreInfo}</li>
+      </ul>
+    </div>
+  </div>`
+    html.push(teammateCard)
+  }
+  html.push(htmlClosingTags)
+  fs.writeFile(`./dist/index.html`, html.join(''), function (err) {
+    if (err) {
+      return console.log(err)
+    }
+    console.log(
+      'index.html has been successfully created and saved to the `dist` directory.'
+    )
+  })
+}
+
+createTeam()
